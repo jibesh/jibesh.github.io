1cc1ad6a605dc89f1ba7427049012127d8ecb48f
Jibesh Patra <jibesh.patra@gmail.com>
2024-04-25 21:08:08 +0200

    Changes to the theme
    Minor changes to the theme and the overall flow

--- my-script.js
+++ my-script.js
@@ -1,5 +1,5 @@
 function select_random_theme() {
-    let given_themes = ["theme_7"];
+    let given_themes = ["theme_1"];
     let selected_theme = given_themes[[Math.floor(Math.random() * given_themes.length)]]
     document.querySelector("html").setAttribute("selected_theme", selected_theme);
 }
