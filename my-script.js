f997dca95f6259446b425e87dfa5e9cc60048922
Jibesh Patra <jibesh.patra@gmail.com>
2026-05-03 17:43:08 +0530

    updated profiles

--- my-script.js
+++ my-script.js
@@ -1,6 +1,6 @@
 function select_theme_based_on_time() {
     let current_hr = (new Date()).getHours();
-    let given_themes = ["hint-of-red", "hint-of-blue"]; // "red", "orange", "blue", "purple", 
+    let given_themes = ["hint-of-blue", "hint-of-red"]; // "red", "orange", "blue", "purple", 
     // let selected_theme = given_themes[[Math.floor(Math.random() * given_themes.length)]] // Random theme
     let selected_theme = given_themes[0];
 
